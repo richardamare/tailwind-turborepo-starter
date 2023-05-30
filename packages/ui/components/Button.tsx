cdbe2cd17dc36f80be2f6c087f3025759cebf085
Richard Amare <50375553+richardamare@users.noreply.github.com>
2023-05-30 19:26:22 +0200

    finish base structure

---
+++ packages/ui/components/Button.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Button: React.FC = () => {
+  return (
+    <button className="w-60 px-3 py-2 rounded text-red-500">
+      Let&apos;s get it !
+    </button>
+  );
+};
